4b3d5d54045c1e4cfe0dc8f00b35e79af0f70001
AsadbekBahromov21 <bahromov1621a@gmail.com>
2024-07-21 22:03:09 +0500

    platforma

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const skeleton = document.querySelector(".skeleton")
 const wrapper = document.querySelector(".wrapper")
 const semore = document.querySelector(".semore")
 const collection = document.querySelector(".collection")
-for(let i = 0; i < 12; i++){
+for (let i = 0; i < 12; i++) {
     let skeletonItem = document.createElement("div")
     skeletonItem.classList.add("skeleton__item")
     skeletonItem.innerHTML = `
@@ -17,63 +17,73 @@ for(let i = 0; i < 12; i++){
 }
 let perPageCount = 6
 let offset = 1
-async function fetchData(api, limit, category){
+let category = "";
+async function fetchData(api, limit, category) {
     let response = await fetch(`${api}/products${category}?limit=${limit}`)
     response
-    .json()
-    .then(res => createCard(res))
-    .catch(err => console.log(err))
-    .finally(()=>{
-        skeleton.style.display = "none"
-    })
+        .json()
+        .then(res => createCard(res))
+        .catch(err => console.log(err))
+        .finally(() => {
+            skeleton.style.display = "none"
+        })
 }
-fetchData(API__URL, perPageCount, "")
+fetchData(API__URL, perPageCount, category)
 
-function createCard(data){
-    while(wrapper.firstChild){
+function createCard(data) {
+    while (wrapper.firstChild) {
         wrapper.firstChild.remove()
     }
     console.log(data.products[0]);
-    data.products.forEach((product)=>{
+    data.products.forEach((product) => {
         let cardItem = document.createElement("div")
         cardItem.classList.add("card")
-        cardItem.innerHTML =`
+        cardItem.innerHTML = `
+        <div class="icon"><i class="fa-solid fa-heart"></i></div>
         <img src=${product.images[0]} alt="">
         <h3>${product.title}</h3>
         <p class= "desck" title ="${product.description}">${product.description}</p>
+            <div class="meta">
+      <img src="${product.meta.qrCode}" alt="">
+      <div class="creat">
+      <p>${product.meta.createdAt}</p>
+      <p>${product.meta.updatedAt}</p>
+  </div>
+  </div>
         <button>Buy now</button>
         `
-       wrapper.appendChild(cardItem)
+        wrapper.appendChild(cardItem)
 
     })
 }
-semore.addEventListener("click", ()=>{
+semore.addEventListener("click", () => {
     offset++
-    fetchData(API__URL, perPageCount*offset)
+    fetchData(API__URL, perPageCount * offset, category)
 })
-async function fetchCategory(api){
-    let  response = await fetch(`${api}/products/category-list`)
+async function fetchCategory(api) {
+    let response = await fetch(`${api}/products/category-list`)
     response
-    .json()
-    .then(res => createCategory(res))
+        .json()
+        .then(res => createCategory(res))
 
 }
 fetchCategory(API__URL)
 
-function createCategory(data){
-    data.forEach((category)=>{
+function createCategory(data) {
+    data.forEach((category) => {
         let list = document.createElement("li")
-        list.className ="item"
-        list.innerHTML =`
+        list.className = "item"
+        list.innerHTML = `
         <data value="/category/${category}">${category}</data>
         `
         collection.appendChild(list)
     })
 }
-collection.addEventListener("click", (e)=>{
-    if(e.target.tagName === "DATA"){
-       let val = e.target.value
-       fetchData(API__URL, perPageCount, val)
+collection.addEventListener("click", (e) => {
+    if (e.target.tagName === "DATA") {
+        let val = e.target.value
+        let category = val;
+        fetchData(API__URL, perPageCount * offset, category)
     }
 })
 
@@ -106,22 +116,27 @@ collection.addEventListener("click", (e)=>{
 
 
 // console.log(navbar);
-function toggleShow (){
+function toggleShow() {
     navbar.classList.toggle("show")
 }
 $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
         },
-        600:{
-            items:2
+        600: {
+            items: 2
         },
-        1000:{
-            items:3
+        1000: {
+            items: 3
         }
     }
 })
+
+
+
+
+
